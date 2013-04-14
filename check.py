ebd2c36b067a9e87728ec9051479162034b28caf
Steve <steve.j.warner@gmail.com>
2013-04-14 17:35:40 -0300

    Update check.py

--- check.py
+++ check.py
@@ -1,33 +1,29 @@
+# check to see if the phone is connected to my router
 def check_for_phone():
 	import urllib.request
 	import urllib.parse
 	
-	# user-set parameters
+	# username and password for my DLINK router
 	username = 'readonly'
 	password = '64eec07f11'
-	router = '192.168.0.1'
+	router_address = '192.168.0.1'
+	# MAC address of my phone
 	target = '00-11-94-ba-49-3c'
 	
 	params = urllib.parse.urlencode({'login_name' : username, 'login_pass' : password, 'login' : 'Log+in'})
 	params = params.encode('utf-8')
-	
-	f = urllib.request.urlopen("http://%s/login.cgi" % router, params)
-	
-	#params = 0
-	#params = params.encode('utf-8')
-			
-	#f = urllib.request.urlopen("http://%s/st_wireless.htm" % router, params)
-	f = urllib.request.urlopen("http://%s/st_wireless.htm" % router)
-	
+	# login page
+	f = urllib.request.urlopen("http://%s/login.cgi" % router_address, params)
+	# connected wireless devices page
+	f = urllib.request.urlopen("http://%s/st_wireless.htm" % router_address)
 	data = f.read().decode('utf-8')
-	
-	result = data.find(target)
-	
+
+# search the HTML source for my MAC address
 	# if find() doesn't find what it's looking for, it returns -1
-	if(result == -1):
-	  result = 0
+	if(data.find(target) == -1):
+	  result = False
 	else:
-	  result = 1
+	  result = True
 	
 	#print(result)
 	return result
