9e5f4eb55c9c224e803ec0ae3bde74440f8bdee9
Steve <steve.j.warner@gmail.com>
2013-04-20 20:32:04 -0300

    simplifcation

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import time
 running = False
 while True:
   # every minute check if phone exists
-  status = check.check_for_phone()
-  if(status == False):
-    print("Phone is not present")
+  if(check.check_for_phone() == False):
+    print("Phone is NOT connected to router")
+    
     # if it doesn't, run motion, only if it isn't running already
     if(running == False):
       print("Startiong motion")
       subprocess.call(['/home/steve/motion/motion', '-c', 'motion.conf', '-l', 'log.txt'])
       running = True
+  
   else:
-    # if it does, kill motion
-    print("Phone is not present")
+    # if phone is present, kill motion
+    print("Phone is connected to router")
     if(running == True):
       print("Killing motion")
       subprocess.call(['pkill', 'motion'])
